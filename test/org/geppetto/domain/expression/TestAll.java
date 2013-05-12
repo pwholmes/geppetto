4de5adecb4b7108f36f30f544345117d6b3c949a
Mitchell <mitchell.aharon@gmail.com>
2013-05-12 07:53:10 -0400

    More tests added

--- test/org/geppetto/domain/expression/TestAll.java
+++ test/org/geppetto/domain/expression/TestAll.java
@@ -9,12 +9,13 @@ import org.junit.runners.Suite;
  * of the tests within its package as well as within any subpackages of its
  * package.
  *
- * @generatedBy CodePro at 5/12/13 6:28 AM
+ * @generatedBy CodePro at 5/12/13 7:33 AM
  * @author user
  * @version $Revision: 1.0 $
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	UnaryExpressionTest.class,
 	ConstantExpressionTest.class,
 	VariantExpressionTest.class,
 })
@@ -25,7 +26,7 @@ public class TestAll {
 	 *
 	 * @param args the command line arguments
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	public static void main(String[] args) {
 		JUnitCore.runClasses(new Class[] { TestAll.class });
--- test/org/geppetto/domain/expression/UnaryExpressionTest.java
+++ test/org/geppetto/domain/expression/UnaryExpressionTest.java
@@ -1,35 +1,37 @@
 package org.geppetto.domain.expression;
 
 import org.geppetto.GeppettoException;
+import org.geppetto.domain.Operator;
 import org.geppetto.domain.declaration.Value;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 /**
- * The class <code>ConstantExpressionTest</code> contains tests for the class <code>{@link ConstantExpression}</code>.
+ * The class <code>UnaryExpressionTest</code> contains tests for the class <code>{@link UnaryExpression}</code>.
  *
- * @generatedBy CodePro at 5/12/13 6:28 AM
+ * @generatedBy CodePro at 5/12/13 7:33 AM
  * @author user
  * @version $Revision: 1.0 $
  */
-public class ConstantExpressionTest {
+public class UnaryExpressionTest {
 	/**
-	 * Run the ConstantExpression(String) constructor test.
+	 * Run the UnaryExpression(Operator,Expression) constructor test.
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
-	public void testConstantExpression_1()
+	public void testUnaryExpression_1()
 		throws Exception {
-		String name = "helloWorld";
+		Operator operator = Operator.ADD;
+		Expression operand = new InputExpression();
 
-		ConstantExpression result = new ConstantExpression(name);
+		UnaryExpression result = new UnaryExpression(operator, operand);
 
 		// add additional test code here
 		// An unexpected exception was thrown in user code while executing this test:
-		//    java.lang.UnsupportedClassVersionError: org/geppetto/domain/expression/ConstantExpression : Unsupported major.minor version 51.0
+		//    java.lang.UnsupportedClassVersionError: org/geppetto/domain/expression/UnaryExpression : Unsupported major.minor version 51.0
 		//       at java.lang.ClassLoader.defineClass1(Native Method)
 		//       at java.lang.ClassLoader.defineClass(ClassLoader.java:634)
 		//       at java.security.SecureClassLoader.defineClass(SecureClassLoader.java:142)
@@ -51,58 +53,39 @@ public class ConstantExpressionTest {
 	}
 
 	/**
-	 * Run the ConstantExpression(Value) constructor test.
+	 * Run the Expression getOperand() method test.
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
-	public void testConstantExpression_2()
+	public void testGetOperand_1()
 		throws Exception {
-		Value value = new Value(1);
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new ConstantExpression(new Value(3)));
 
-		ConstantExpression result = new ConstantExpression(value);
+		Expression result = fixture.getOperand();
 
 		// add additional test code here
-		// An unexpected exception was thrown in user code while executing this test:
-		//    java.lang.UnsupportedClassVersionError: org/geppetto/domain/expression/ConstantExpression : Unsupported major.minor version 51.0
-		//       at java.lang.ClassLoader.defineClass1(Native Method)
-		//       at java.lang.ClassLoader.defineClass(ClassLoader.java:634)
-		//       at java.security.SecureClassLoader.defineClass(SecureClassLoader.java:142)
-		//       at java.net.URLClassLoader.defineClass(URLClassLoader.java:277)
-		//       at java.net.URLClassLoader.access$000(URLClassLoader.java:73)
-		//       at java.net.URLClassLoader$1.run(URLClassLoader.java:212)
-		//       at java.security.AccessController.doPrivileged(Native Method)
-		//       at java.net.URLClassLoader.findClass(URLClassLoader.java:205)
-		//       at java.lang.ClassLoader.loadClass(ClassLoader.java:321)
-		//       at com.instantiations.assist.eclipse.junit.execution.core.UserDefinedClassLoader.loadClass(UserDefinedClassLoader.java:62)
-		//       at java.lang.ClassLoader.loadClass(ClassLoader.java:266)
-		//       at com.instantiations.assist.eclipse.junit.execution.core.ExecutionContextImpl.getClass(ExecutionContextImpl.java:99)
-		//       at com.instantiations.eclipse.analysis.expression.model.SimpleTypeExpression.execute(SimpleTypeExpression.java:205)
-		//       at com.instantiations.eclipse.analysis.expression.model.InstanceCreationExpression.execute(InstanceCreationExpression.java:425)
-		//       at com.instantiations.assist.eclipse.junit.execution.core.ExecutionRequest.execute(ExecutionRequest.java:286)
-		//       at com.instantiations.assist.eclipse.junit.execution.communication.LocalExecutionClient$1.run(LocalExecutionClient.java:158)
-		//       at java.lang.Thread.run(Thread.java:679)
-		assertNotNull(result);
+		assertEquals(result.getClass(), ConstantExpression.class);
 	}
 
 	/**
-	 * Run the String getName() method test.
+	 * Run the Operator getOperator() method test.
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
-	public void testGetName_1()
+	public void testGetOperator_1()
 		throws Exception {
-		ConstantExpression fixture = new ConstantExpression("hello");
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new ConstantExpression(new Value("stringValue")));
 
-		String result = fixture.getName();
+		Operator result = fixture.getOperator();
 
 		// add additional test code here
-		assertEquals(result, "hello");
+		assertEquals(result, Operator.ADD);
 	}
 
 	/**
@@ -110,30 +93,50 @@ public class ConstantExpressionTest {
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
 	public void testGetValue_1()
 		throws Exception {
-		ConstantExpression fixture = new ConstantExpression("");
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new ConstantExpression( new Value(1)));
 
-		Value result = fixture.getValue();
+		ConstantExpression result = (ConstantExpression) fixture.getOperand();
 
 		// add additional test code here
-		assertNull(result);
+		assertEquals(result.getValue().getIntValue(), 1);
 	}
+
+	/**
+	 * Run the Value getValue() method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
+	 */
+	@Test
+	public void testGetValue_2()
+		throws Exception {
+		UnaryExpression fixture = new UnaryExpression(Operator.UNARY_MINUS, new ConstantExpression( new Value(true) ));
+
+		ConstantExpression result = (ConstantExpression) fixture.getOperand();
+
+		// add additional test code here
+		assertTrue(result.getValue().getBooleanValue());
+	}
+
+
 
 	/**
 	 * Run the boolean isLValue() method test.
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
 	public void testIsLValue_1()
 		throws Exception {
-		ConstantExpression fixture = new ConstantExpression("someString");
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new VariableExpression("foo"));
 
 		boolean result = fixture.isLValue();
 
@@ -146,17 +149,18 @@ public class ConstantExpressionTest {
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test(expected = org.geppetto.GeppettoException.class)
 	public void testSetValue_1()
 		throws Exception {
-		ConstantExpression fixture = new ConstantExpression("");
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new InputExpression());
 		Value value = new Value(1);
 
 		fixture.setValue(value);
 
 		// add additional test code here
+		assertEquals(fixture.getValue().getIntValue(), 1);
 	}
 
 	/**
@@ -164,12 +168,12 @@ public class ConstantExpressionTest {
 	 *
 	 * @throws Exception
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Test
 	public void testToString_1()
 		throws Exception {
-		ConstantExpression fixture = new ConstantExpression("");
+		UnaryExpression fixture = new UnaryExpression(Operator.ADD, new InputExpression());
 
 		String result = fixture.toString();
 
@@ -183,7 +187,7 @@ public class ConstantExpressionTest {
 	 * @throws Exception
 	 *         if the initialization fails for some reason
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@Before
 	public void setUp()
@@ -197,7 +201,7 @@ public class ConstantExpressionTest {
 	 * @throws Exception
 	 *         if the clean-up fails for some reason
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	@After
 	public void tearDown()
@@ -210,9 +214,9 @@ public class ConstantExpressionTest {
 	 *
 	 * @param args the command line arguments
 	 *
-	 * @generatedBy CodePro at 5/12/13 6:28 AM
+	 * @generatedBy CodePro at 5/12/13 7:33 AM
 	 */
 	public static void main(String[] args) {
-		new org.junit.runner.JUnitCore().run(ConstantExpressionTest.class);
+		new org.junit.runner.JUnitCore().run(UnaryExpressionTest.class);
 	}
 }
